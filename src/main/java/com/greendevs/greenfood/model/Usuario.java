5350ce26ce58f3b5ae3bbe92dffe6c910fa32053
Gustavo Balieiro <gbalieirof@gmail.com>
2023-02-16 10:07:03 -0300

    Deploy do Projeto Greenfood

--- src/main/java/com/greendevs/greenfood/model/Usuario.java
+++ src/main/java/com/greendevs/greenfood/model/Usuario.java
@@ -1,5 +1,6 @@
 package com.greendevs.greenfood.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -19,7 +20,7 @@ public class Usuario {
 
     @NotNull(message = "O atributo nome é obrigatório.")
     private String nome;
-
+    @Schema(example = "email@email.com.br")
     @NotNull(message = "O atributo usuário é obrigatório.")
     @Email(message = "O atributo usuário deve ter um email válido.")
     private String usuario;
